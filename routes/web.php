651e6931905198da59bb30dd7622c60e7034a7d5
fajarpp08 <fajarpp801@gmail.com>
2024-07-05 17:13:00 +0700

    testing env

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAccess;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -13,41 +17,41 @@ Route::get('/', function () {
     return view('home');
 });
 
-// Route::middleware(['auth'])->group(function () {
-//     Route::get('logout', [AuthController::class, 'logout']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('logout', [AuthController::class, 'logout']);
 
-//     Route::middleware([AuthAccess::class . ':Admin'])->group(function () {
-//         // ROUTE Admin Access only
-//         Route::get('/dashboardadmin', [DashboardController::class, 'dashboardAdmin'])->name('dashboardAdmin');
+    Route::middleware([AuthAccess::class . ':Admin'])->group(function () {
+        // ROUTE Admin Access only
+        Route::get('/dashboardadmin', [DashboardController::class, 'dashboardAdmin'])->name('dashboardAdmin');
 
-//         Route::resource('useradm', UserController::class);
-//         Route::resource('mobil', MobilController::class);
-//         Route::resource('sewa', SewaController::class);
-//         Route::resource('pengembalian', PengembalianController::class);
-//         // // laporan sewa
-//         Route::get('/laporansewa', [SewaController::class, 'laporan'])->name('laporansewa');
-//         Route::post('/cetak_laporansewa', [SewaController::class, 'cetakLaporan'])->name('cetak_laporansewa');
-//         // // laporan pengembalian
-//         Route::get('/laporanpengembalian', [PengembalianController::class, 'laporan'])->name('laporanpengembalian');
-//         Route::post('/cetak_laporanpengembalian', [PengembalianController::class, 'cetakLaporan'])->name('cetak_laporanpengembalian');
-//     });
+        Route::resource('useradm', UserController::class);
+        // Route::resource('mobil', MobilController::class);
+        // Route::resource('sewa', SewaController::class);
+        // Route::resource('pengembalian', PengembalianController::class);
+        // // laporan sewa
+        // Route::get('/laporansewa', [SewaController::class, 'laporan'])->name('laporansewa');
+        // Route::post('/cetak_laporansewa', [SewaController::class, 'cetakLaporan'])->name('cetak_laporansewa');
+        // // laporan pengembalian
+        // Route::get('/laporanpengembalian', [PengembalianController::class, 'laporan'])->name('laporanpengembalian');
+        // Route::post('/cetak_laporanpengembalian', [PengembalianController::class, 'cetakLaporan'])->name('cetak_laporanpengembalian');
+    });
 
-//     // ROUTE User / Login Access Only
-//     Route::get('/dashboarduser', [DashboardController::class, 'dashboardUser'])->name('dashboardUser');
-//     Route::get('/mobiluser', [DashboardController::class, 'mobilUser'])->name('mobilUser');
-//     Route::get('/mobil/detail/{id}', [DashboardController::class, 'mobilDetail'])->name('mobil.detail');
-//     Route::get('/account', [DashboardController::class, 'account'])->name('account');
+    // ROUTE User / Login Access Only
+    Route::get('/dashboarduser', [DashboardController::class, 'dashboardUser'])->name('dashboardUser');
+    // Route::get('/mobiluser', [DashboardController::class, 'mobilUser'])->name('mobilUser');
+    // Route::get('/mobil/detail/{id}', [DashboardController::class, 'mobilDetail'])->name('mobil.detail');
+    // Route::get('/account', [DashboardController::class, 'account'])->name('account');
 
-//     // Peminjaman User
-//     Route::get('/rental/form/{mobil_id}', [SewaController::class, 'formSewa'])->name('rental.form');
-//     Route::post('/rental/create', [SewaController::class, 'createSewa'])->name('rental.create');
-//     Route::get('/rentalan', [SewaController::class, 'rentalan'])->name('rentalan');
-//     Route::post('/kembalikan/{rental}', [SewaController::class, 'kembalikan'])->name('kembalikan');
+    // Peminjaman User
+    // Route::get('/rental/form/{mobil_id}', [SewaController::class, 'formSewa'])->name('rental.form');
+    // Route::post('/rental/create', [SewaController::class, 'createSewa'])->name('rental.create');
+    // Route::get('/rentalan', [SewaController::class, 'rentalan'])->name('rentalan');
+    // Route::post('/kembalikan/{rental}', [SewaController::class, 'kembalikan'])->name('kembalikan');
 
-//     // Route fitur search
-//     Route::get('/mobils/search', [MobilController::class, 'searchByName'])->name('mobils.searchname');
-//     Route::get('/searchmobil', [MobilController::class, 'searchByDate'])->name('mobils.searchdate');
-// });
+    // Route fitur search
+    // Route::get('/mobils/search', [MobilController::class, 'searchByName'])->name('mobils.searchname');
+    // Route::get('/searchmobil', [MobilController::class, 'searchByDate'])->name('mobils.searchdate');
+});
 
 // Route Register
 // Route::get('/register', [AuthController::class, 'register'])->name('register');
